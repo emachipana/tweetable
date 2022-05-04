afe72b0ac5dafb9452f1b6f040e703196de8436f
Enmanuel <emachipana0102@gmail.com>
2022-05-04 18:31:16 -0500

    CHORE: setup devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   resources :likes
   resources :tweets
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "tweets#index"
 end
