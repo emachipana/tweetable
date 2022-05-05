086a1acac7f7259b9c33894b2245af74a000bdc1
MaxAgui <manuelmaximo19@gmail.com>
2022-05-04 21:44:57 -0500

    tweet

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,7 +1,7 @@
 class Tweet < ApplicationRecord
-  belongs_to :user
-  has_many :replies, class_name: "Tweet",foreign_key: "parent_id", dependent: :destroy, inverse_of: "replied_to"
-  belongs_to :replied_to. class_name: "Tweet",optional: true
+  belongs_to :user 
+  has_many :replies, class_name: "Tweet",foreign_key: "replied_to_id", dependent: :destroy, inverse_of: "replied_to"
+  belongs_to :replied_to, class_name: "Tweet",optional: true
   has_many :likes
   has_many :users, through: :likes
 end
