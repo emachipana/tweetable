53927ff3b7de53eacaeedfe3c167bb6e87b5af7f
MaxAgui <manuelmaximo19@gmail.com>
2022-05-04 21:03:59 -0500

    ADD: associations

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,7 @@
 class Tweet < ApplicationRecord
   belongs_to :user
-  belongs_to :replied_to
+  has_many :replies, class_name: "Tweet",foreign_key: "parent_id", dependent: :destroy, inverse_of: "replied_to"
+  belongs_to :replied_to. class_name: "Tweet",optional: true
+  has_many :likes
+  has_many :users, through: :likes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  # has_and_belongs_to_many :likes, :join_table => 'likes', :class_name => 'Tweet'
+  # has_many :tweets
+
+  has_many :tweets, :through => :likes
+  has_many :likes
+  has_many :tweets
+
+
+  enum :role, { admin:0 ,regular:1 }
 end
