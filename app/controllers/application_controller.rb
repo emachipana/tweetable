bf5047bd3227665b016af1c4bff99942470b9e1a
Enmanuel <emachipana0102@gmail.com>
2022-05-05 09:33:39 -0500

    ADD: edit user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[username name avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[username name avatar])
   end
 end
