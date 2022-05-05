6b1d661f7058cd4e9af41547af3bd083101e8766
Enmanuel <emachipana0102@gmail.com>
2022-05-04 19:36:02 -0500

    ADD: method from_omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable
+  
+
+  def self.from_omniauth(auth_hash)
+    where(provider: auth_hash.provider, uid: auth_hash.uid).first_or_create do |user|
+      user.provider = auth_hash.provider
+      user.uid = auth_hash.uid
+      user.username = auth_hash.info.nicknamne
+      user.email = auth_hash.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end
