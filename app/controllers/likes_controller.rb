6308efd1391c9ccce90312f9bf80de77cfb92bc8
Enmanuel <emachipana0102@gmail.com>
2022-05-05 19:13:45 -0500

    ADD: method like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,57 +1,15 @@
 class LikesController < ApplicationController
-  before_action :set_like, only: %i[show edit update destroy]
-
-  # GET /likes
-  def index
-    @likes = Like.all
-  end
-
-  # GET /likes/1
-  def show; end
-
-  # GET /likes/new
-  def new
-    @like = Like.new
-  end
-
-  # GET /likes/1/edit
-  def edit; end
-
   # POST /likes
   def create
-    @like = Like.new(like_params)
+    @tweet = Tweet.find_by(id: params[:tweet_id])
+    @like = @tweet.likes.new(user: current_user)
 
     if @like.save
-      redirect_to @like, notice: "Like was successfully created."
+      redirect_to tweets_path
     else
-      render :new, status: :unprocessable_entity
+      like = Like.find_by(user: current_user, tweet: @tweet)
+      like.destroy
+      redirect_to tweets_path
     end
-  end
-
-  # PATCH/PUT /likes/1
-  def update
-    if @like.update(like_params)
-      redirect_to @like, notice: "Like was successfully updated."
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /likes/1
-  def destroy
-    @like.destroy
-    redirect_to likes_url, notice: "Like was successfully destroyed."
-  end
-
-  private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_like
-    @like = Like.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def like_params
-    params.require(:like).permit(:user_id, :tweet_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: :callbacks }
-  resources :likes
-  resources :tweets
+  resources :tweets do
+    resources :likes, only: [:create]
+  end
 
   root "tweets#index"
   get "/profile", to: "users#profile"
